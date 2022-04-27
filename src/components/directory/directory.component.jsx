91e5f54453be3bc80ea5c5f51f55275483187fc4
Cuong Phan <codyreactista@gmail.com>
2022-04-28 00:15:07 +0700

    moving directory and category-item

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from '../category-item/category-item.component';
+import './directory.styles.scss';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map(({ id, ...category }) => (
+        <CategoryItem key={id} {...category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
